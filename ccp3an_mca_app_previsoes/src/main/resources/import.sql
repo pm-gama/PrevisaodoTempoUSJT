7185b31d5409c78bf0559b6f2534f1d7a05a3ab5
pm-gama <57262085+pm-gama@users.noreply.github.com>
2019-11-02 11:41:01 -0300

    Aula 02
    Entrega da Aula 02

--- ccp3an_mca_app_previsoes/src/main/resources/import.sql
+++ ccp3an_mca_app_previsoes/src/main/resources/import.sql
@@ -3,3 +3,6 @@
 insert into tempo (id, dia, Min, Max, ar, tex) values (1, 'Quarta', 77, 774, 87, 'Sol');
 insert into tempo (id, dia, Min, Max, ar, tex) values (2, 'Quinta', 33, 999, 66, 'Sol entre Nuvens');
 insert into tempo (id, dia, Min, Max, ar, tex) values (3, 'Sexta', 1, 774, 77, 'Nublado');
+
+--adicione um usuário
+insert into usuario (id, login, senha) values (1, 'admin', 'admin');
