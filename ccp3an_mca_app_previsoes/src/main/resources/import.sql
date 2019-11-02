4b10cee5c7c23203f9ccf01a513fa93420b4f02e
pm-gama <57262085+pm-gama@users.noreply.github.com>
2019-11-02 11:21:28 -0300

    Aula 01
    Exercício 01

---
+++ ccp3an_mca_app_previsoes/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+
+
+insert into tempo (id, dia, Min, Max, ar, tex) values (1, 'Quarta', 77, 774, 87, 'Sol');
+insert into tempo (id, dia, Min, Max, ar, tex) values (2, 'Quinta', 33, 999, 66, 'Sol entre Nuvens');
+insert into tempo (id, dia, Min, Max, ar, tex) values (3, 'Sexta', 1, 774, 77, 'Nublado');
