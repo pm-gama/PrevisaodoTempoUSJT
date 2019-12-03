c328f84930562c20f74af52748e66651cc3d824d
pm-gama <57262085+pm-gama@users.noreply.github.com>
2019-12-03 12:08:58 -0300

    Revert "Aula 04.1"
    This reverts commit 1c8418d53bf3f1fc0768c48045830de50f2b23ed.

--- ccp3an_mca_app_previsoes/ccp3an_mca_app_previsoes/src/main/java/br/usjt/hellospringboot/repository/DiaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.usjt.hellospringboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.usjt.hellospringboot.model.Dia;
-
-
-public interface DiaRepository extends JpaRepository<Dia, Long>{
-
-}
